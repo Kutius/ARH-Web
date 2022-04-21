38ad2a84b4fbd70b3060e4f466adfc641e088de2
Gaaraly <1205913452@qq.com>
2022-04-22 00:39:39 +0800

    chore:登录布局

--- src/layouts/login.vue
+++ src/layouts/login.vue
@@ -1,26 +1,29 @@
 <template>
 	<div class="bg-view">
-		<div class="bg-view-container">123</div>
+		<div class="bg-view-container bg-blue-400/40 shadow">
+			<router-view />
+		</div>
 	</div>
-	<router-view />
 </template>
 
 <style lang="less" scoped>
 .bg-view {
 	display: flex;
-	// flex: auto;
 	flex-direction: column;
-	// align-items: center;
+	align-items: center;
+	justify-content: center;
 	height: 100vh;
 	overflow: auto;
-	background-color: rgb(15, 62, 62);
+	background-image: url(/assets/login.svg);
+	background-repeat: no-repeat;
+	background-position: 50%;
+	background-size: 100%;
+
 	&-container {
-		padding: 20px 0;
-		margin: 0 auto;
-		flex: 1;
-		width: 200px;
-		height: 200px;
-		background-color: rgb(255, 255, 255);
+		border-radius: 10px;
+		padding: 20px 15px;
+		width: 400px;
+		height: 340px;
 	}
 }
 </style>
