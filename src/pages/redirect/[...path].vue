b41bb94a8afb240eafe40970f39dd1e42a3be003
Gaaraly <1205913452@qq.com>
2022-05-08 15:50:51 +0800

    fix:导航栏刷新

---
+++ src/pages/redirect/[...path].vue
@@ -0,0 +1,9 @@
+<template></template>
+<script lang="ts">
+export default {
+	beforeRouteEnter(to: any, from: any, next: any) {
+		console.log(from.fullPath)
+		next(from.fullPath)
+	},
+}
+</script>
