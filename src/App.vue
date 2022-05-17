e2ab958efbe727388b597558d54bb09d82c8d8e8
Gaaraly <1205913452@qq.com>
2022-05-17 15:34:47 +0800

    chore:ui组件i18n

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { darkTheme } from 'naive-ui'
+import { zhCN, enUS } from 'naive-ui'
+
 const { isDark } = useDarks()
+const { locale } = useI18n()
 const theme = computed(() => (isDark.value ? darkTheme : null))
+const uiLocale = computed(() => (locale.value === 'zh-CN' ? zhCN : enUS))
 </script>
 
 <template>
 	<n-message-provider>
-		<n-config-provider :theme="theme">
+		<n-config-provider :theme="theme" :locale="uiLocale">
 			<router-view />
 		</n-config-provider>
 	</n-message-provider>
