21c50b114ec367c91c05f779df87789feb713965
Gaaraly <1205913452@qq.com>
2022-05-18 21:03:44 +0800

    chore:用户接口

--- src/api/patient.ts
+++ src/api/patient.ts
@@ -1,10 +1,17 @@
 import service from '~/composables/axios'
 import type { IResponseData } from './types/common'
-import type { IApmtDetailResponse } from './types/patient'
+import type { IApmtDetailResponse, ISpaceInfoResponse } from './types/patient'
 
 export const apmtDetail = () => {
 	return service<IResponseData<IApmtDetailResponse>>({
 		method: 'POST',
 		url: '/api/apmtDetail',
+	})
+}
+
+export const userSpace = () => {
+	return service<IResponseData<ISpaceInfoResponse>>({
+		method: 'POST',
+		url: '/api/userSpace',
 	})
 }
--- src/api/types/patient.ts
+++ src/api/types/patient.ts
@@ -11,3 +11,21 @@ export interface IApmtDetail {
 export interface IApmtDetailResponse {
 	apmt: IApmtDetail[]
 }
+
+export interface IApmtDetailData {
+	department: string
+	timestamp: number
+}
+
+enum sex {
+	male,
+	female,
+}
+
+export interface ISpaceInfoResponse {
+	id: string
+	name: string
+	phone: string
+	idNumber: string
+	sex: sex
+}
