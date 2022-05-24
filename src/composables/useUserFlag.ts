4d2d0a8fd2a52880d6e31d983bfd900a788eb8a4
Gaaraly <1205913452@qq.com>
2022-05-24 19:47:58 +0800

    chore:userflag逻辑完善

--- src/composables/useUserFlag.ts
+++ src/composables/useUserFlag.ts
@@ -3,7 +3,7 @@ import { userSpace } from '~/api/patient'
 export default () => {
 	const flag = ref<boolean>(false)
 
-	const getUserSpace = async (callback: any) => {
+	const getUserSpace = async (callback?: any) => {
 		const res = await userSpace()
 		if (res.code === 0) {
 			if (!(res.data.name && res.data.idNumber)) {
